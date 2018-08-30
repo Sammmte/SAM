fb9eca90d138a239bed23e30797fda7d34646fa6
Sammmte <santiagoperez.-@hotmail.com>
2018-08-30 09:33:52 -0300

    Creado un timer sincronico muy sencillo.

---
+++ SAM/SAM/Timers/SyncTimer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace SAM.Timers
+{
+    public class SyncTimer
+    {
+        public float Interval { get; set; }
+        public bool Loop { get; set; }
+
+        private float currentTime;
+        private bool active;
+        
+        public event Action<SyncTimer> onTick;
+
+        public SyncTimer()
+        {
+
+        }
+
+        public void Start()
+        {
+            currentTime = 0;
+            active = true;
+        }
+
+        public void Update(float elapsed)
+        {
+            if(active)
+            {
+                currentTime += elapsed;
+
+                if (currentTime >= Interval)
+                {
+                    if(onTick != null)
+                    {
+                        onTick(this);
+                    }
+
+                    if (!Loop)
+                    {
+                        Stop();
+                    }
+                    else
+                    {
+                        currentTime = 0;
+                    }
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            active = false;
+        }
+    }
+}
